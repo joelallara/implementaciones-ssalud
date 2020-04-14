6d1114a2f1e95854f7473986d51cc0b338c1a62d
Joel Allara <joel_allara@hotmail.com>
2020-04-13 23:05:09 -0300

    Actualizaciones en Login: Redireccion a Home cuando inicia sesion; redireccion a login cuando cierra sesion

--- Implementaciones/implementaciones/settings.py
+++ Implementaciones/implementaciones/settings.py
@@ -124,3 +124,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#Auth redirect
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
+
+
+# Emails
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+else:
+    # Configuracion de email real para produccion
+    pass
+
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
