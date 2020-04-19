69c823de1ff33435100fa763a70f8c54b00c7dce
Joel Allara <joel_allara@hotmail.com>
2020-04-19 18:08:36 -0300

    Correcciones en comportamiento del modal 'Nueva Solicitud'

--- Implementaciones/project/admin.py
+++ Implementaciones/project/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
-from .models import Project, Package
+from .models import Project, Package, Task
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('project_name', 'created', 'modified', 'actived', 'reason', 'disabled_date')
+    list_display = ('project_name', 'created', 'modified',
+                    'actived', 'reason', 'disabled_date')
 
 
 class PackageAdmin(admin.ModelAdmin):
-    list_display = ('package_name', 'created', 'modified', 'actived', 'reason', 'disabled_date')
+    list_display = ('package_name', 'created', 'modified',
+                    'actived', 'reason', 'disabled_date')
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task_name', 'created', 'modified',
+                    'actived', 'reason', 'disabled_date')
 
 
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Package, PackageAdmin)
+admin.site.register(Task, TaskAdmin)
--- Implementaciones/request/static/request/js/request.js
+++ Implementaciones/request/static/request/js/request.js
@@ -18,9 +18,14 @@ class SelectPicker {
     return $(this.id + ' option:selected').text();
   }
 
+  setValueOptionSelected(value) {
+    $(this.id).val(value);
+  }
+
   // Verify if any option is selected
   isOptionSelected() {
-    if (this.getValueOptionSelected == "") {
+    var optionSelected = this.getValueOptionSelected();
+    if (optionSelected == '') {
       return false;
     } else {
       return true;
@@ -63,6 +68,7 @@ class SelectPicker {
           selectPicker.setTitle('-----');
         } else {
           selectPicker.setTitle('Información inexistente');
+          selectPicker.disable();
         }
         $(selectPicker.id).html(rows);
         selectPicker.refresh();
@@ -98,18 +104,21 @@ $(document).ready(function () {
     var isProjectSelected = projectsSelectPicker.isOptionSelected();
 
     // Enable or disable "Agregar" button if there is a project selected
-    if (!isProjectSelected) {
-      $("span.project").addClass("error");
-      $(".add-new").attr("disabled", "disabled");
-      //return;
-    } else {
+    if (isProjectSelected) {
       var url = '/proyectos/' + projectsSelectPicker.getIdOptionSelected() + '/paquetes';
       $("span.project").removeClass("error");
       $(".add-new").removeAttr("disabled");
+
+
+      tasksSelectPicker.setTitle('-----');
+      tasksSelectPicker.refresh();
+
       packagesSelectPicker.url = url;
       packagesSelectPicker.fill();
       packagesSelectPicker.enable();
       packagesSelectPicker.refresh();
+
+      
     }
   });
 
@@ -145,15 +154,20 @@ $(document).ready(function () {
       projectsSelectPicker.disable();
       projectsSelectPicker.refresh();
     } else {
-      // If not project selected exit
+      $("span.project").addClass("error");
+      $(".add-new").attr("disabled", "disabled");
+      //return;
       return;
     }
 
-    var observation = $.trim($("#observaciones").val());
+    var observation = $.trim($("#observacionesInput").val());
     var index = $("#table-add tbody tr:last-child").index();
-    var selectedPackage = packagesSelectPicker.getValueOptionSelected(); $("#packageSelectpicker option:selected").text();
+    // var selectedProject = packagesSelectPicker.getValueOptionSelected();
+    var selectedProject = $(projectsSelectPicker.id + ' option:selected').val();
+    var selectedPackage = packagesSelectPicker.getValueOptionSelected();
     var selectedTasks = $(tasksSelectPicker.id).find('option:selected');
     var values = [];
+
     selectedTasks.each(function () {
       values.push('<li>' + $(this).text());
     });
@@ -181,10 +195,19 @@ $(document).ready(function () {
       $('#table-add').show();
     }
 
+    
+    // Reset form
     $('#implementationRequestForm').trigger("reset");
+
+    // Keeps the value of the selected project
+    projectsSelectPicker.setValueOptionSelected(selectedProject);
+
+    // Refresh selectpickers
+    projectsSelectPicker.refresh();
     packagesSelectPicker.refresh();
     tasksSelectPicker.refresh();
-    $('select[name=projectSelectpicker]').val(selectedProject);
+
+
   });
 
   // Delete row from add-table
@@ -204,201 +227,6 @@ $(document).ready(function () {
   });
 
 });
-
-//Fill the provided Selectpicker
-// function fillSelectPicker(selectPicker) {
-//   if (selectPicker == 'projects') {
-//     var url = $('#projectSelectpicker').data('url');
-//     var selectPickerId = "#projectSelectpicker";
-//   } else if (selectPicker == 'packages') {
-//     var projectIdSelected = $('#projectSelectpicker option:selected').val();
-//     var url = '/proyectos/' + projectIdSelected + '/paquetes';
-//     var selectPickerId = "#packageSelectpicker";
-//   } else if (selectPicker == 'tasks') {
-//     var packageId = $('#packageSelectpicker option:selected').val();
-//     var url = '/proyectos/' + packageId + '/tareas';
-//     var selectPickerId = "#tasksSelectpicker";
-//   }
-
-//   $.ajax({
-//     url: url,
-//     type: 'get',
-//     dataType: 'json',
-//     success: function (data) {
-//       let rows = '';
-//       if (data[selectPicker]) {
-//         if (selectPicker == 'projects') {
-//           data.projects.forEach(project => {
-//             rows += `<option value="${project.id}">${project.project_name}</option>`;
-//           });
-//         } else if (selectPicker == 'packages') {
-//           data.packages.forEach(package => {
-//             rows += `<option value="${package.id}">${package.package_name}</option>`;
-//           });
-//         } else if (selectPicker == 'tasks') {
-//           data.tasks.forEach(task => {
-//             rows += `<option value="${task.id}">${task.task_name}</option>`;
-//           });
-//         }
-//         $(selectPickerId).selectpicker({
-//           title: '-----',
-//         });
-//       } else {
-//         $(selectPickerId).selectpicker({
-//           title: 'Información inexistente',
-//         });
-//       }
-
-//       $(selectPickerId).html(rows);
-//       $(selectPickerId).selectpicker('refresh');
-//     }
-//   });
-// }
-
-//Fills the project selectPicker
-// fillSelectPicker('projects');
-
-
-// // Disable Package and Tasks Selecpicker
-// $("#packageSelectpicker").prop('disabled', true);
-// $('#packageSelectpicker').selectpicker('refresh');
-// $("#tasksSelectpicker").prop('disabled', true);
-// $('#tasksSelectpicker').selectpicker('refresh');
-
-// // Add selectpicker bootstrap style and props
-// $('select').selectpicker({
-//   noneResultsText: 'No se encontraron resultados'
-// });
-
-// Gets the value of the project selectpicker
-// var selectedProject = $("#projectSelectpicker option:selected").text();
-// $("#projectSelectpicker").change(function () {
-//   fillSelectPicker('packages');
-//   selectedProject = $("#projectSelectpicker option:selected").text();
-//   projectSelect(selectedProject);
-//   $("#packageSelectpicker").prop('disabled', false);
-//   $('#packageSelectpicker').selectpicker('refresh');
-// });
-
-// Allows Tasks selectpicker when a package is selected
-// $("#packageSelectpicker").change(function () {
-//   fillSelectPicker('tasks');
-//   $("#tasksSelectpicker").prop('disabled', false);
-//   $('#tasksSelectpicker').selectpicker('refresh');
-// });
-
-
-
-// // Gets the value of the tasks selectpicker
-// // selectedTasks = $("#tasksSelectpicker option:selected").text();
-// // $("#tasksSelectpicker").change(function () {
-// //   selectedTasks = $("#tasksSelectpicker option:selected").text();
-// //   console.log(selectedTasks)
-// // });
-
-
-
-
-// Verify if there is any row on #add-table
-// var isTableEmpty = function () {
-//   var count = $("#table-add tbody tr").length;
-//   if (count >= 1) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-
-// // Verify if any project is selected
-// var isProjectSelected = function (selectedProject) {
-//   if (selectedProject == "") {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-
-// // Enable or disable "Agregar" button if there is a project selected
-// var projectSelect = function (selectedProject) {
-//   if (!isProjectSelected(selectedProject)) {
-//     $("span.project").addClass("error");
-//     $(".add-new").attr("disabled", "disabled");
-//     return;
-//   } else {
-//     $("span.project").removeClass("error");
-//     $(".add-new").removeAttr("disabled");
-//   }
-// };
-
-// // Append table with add row form on add new button click
-// $(".add-new").click(function () {
-//   var project = projectSelect(selectedProject)
-//   $('.container-fluid').find(".error").focus();
-
-//   //Check if there is a project selected on selectpicker
-//   if (isProjectSelected(project)) {
-//     $(".add-new").removeAttr("disabled");
-//     $("#projectSelectpicker").prop('disabled', true);
-//     $('#projectSelectpicker').selectpicker('refresh');
-//   } else {
-//     return;
-//   }
-
-//   var observation = $.trim($("#observaciones").val());
-//   var index = $("#table-add tbody tr:last-child").index();
-//   var selectedPackage = $("#packageSelectpicker option:selected").text();
-//   var selectedTasks = $("#tasksSelectpicker").find('option:selected');
-//   var values = [];
-//   selectedTasks.each(function () {
-//     values.push('<li>' + $(this).text());
-//   });
-//   tasks = values.join("</li>");
-//   var row = '<tr>' +
-//     '<td class="text-center align-middle">' +
-//     selectedPackage +
-//     '</td>' +
-//     '<td class="text-left align-middle">' +
-//     '<ol>' +
-//     tasks +
-//     '<ol>' +
-//     '</td>' +
-//     '<td class="text-justify align-middle">' +
-//     observation +
-//     '</td>' +
-//     '<td class="text-center align-middle">' +
-//     '<a class="delete" title="Delete"><i class="material-icons">&#xE872;</i></a>' +
-//     '</td>' +
-//     '</tr>';
-//   $("#table-add").append(row);
-//   $("#table-add tbody tr").eq(index + 4).find(".delete").toggle();
-
-//   if (!isTableEmpty()) {
-//     $('#table-add').show();
-//   }
-
-//   $('#implementationRequestForm').trigger("reset");
-//   $("#packageSelectpicker").selectpicker("refresh");
-//   $("#tasksSelectpicker").selectpicker("refresh");
-//   $('select[name=projectSelectpicker]').val(selectedProject);
-// });
-
-// // Delete row from add-table
-// var deleteRow = function (row) {
-//   row.remove();
-// };
-
-// // Delete row on delete button click
-// $(document).on("click", ".delete", function () {
-//   var row = $(this).parents("tr");
-//   deleteRow(row);
-
-//   if (isTableEmpty() == true) {
-//     $("#projectSelectpicker").prop('disabled', false);
-//     $('#projectSelectpicker').selectpicker('refresh');
-//   }
-// });
-
-// });
 
 //Fill Details Modal
 function fillDetailsModal(el) {
